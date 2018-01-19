8dbd5688e8f794e6d61158ac00fb87a0f6120592
Mike Kearney <kearneymw@missouri.edu>
2018-01-19 10:57:19 -0600

    now passes most beamer args and added more style tweaks

--- R/uslides.R
+++ R/uslides.R
@@ -3,34 +3,46 @@
 #' Document template for uslide beamer presentation.
 #'
 #' @export
-uslides_document <- function() {
+uslides_document <- function(toc = FALSE,
+                             incremental = FALSE,
+                             fig_width = 9,
+                             fig_height = 6,
+                             fig_crop = TRUE,
+                             fig_caption = TRUE,
+                             keep_tex = FALSE,
+                             pandoc_args = NULL,
+                             highlight = "tango",
+                             latex_engine = "xelatex") {
   template_path <- system.file(
     "rmarkdown", "templates", "uslides_document", package = "uslides"
   )
-  ## set locations of css and doc pre/suf fixes
+  ## set locations of doc pre/suf fixes
   doc_prefix <- file.path(
     template_path, "resources", "uslides_document_prefix.tex"
   )
   doc_prefix <- add_logos(doc_prefix)
-  doc_prebody <- file.path(
-    template_path, "resources", "uslides_document_prebody.tex"
-  )
   doc_afterbody <- file.path(
     template_path, "resources", "uslides_document_afterbody.tex"
   )
   ## call the base html_document function
   rmarkdown::beamer_presentation(
-    latex_engine = "xelatex",
+    toc = toc,
+    incremental = incremental,
+    theme = "boxes",
+    colortheme = "structure",
+    latex_engine = latex_engine,
     df_print = "tibble",
-    fig_crop = FALSE,
-    fig_width = 9,
-    fig_height = 6,
-    highlight = "haddock",
+    fig_crop = fig_crop,
+    fig_width = fig_width,
+    fig_height = fig_height,
+    fig_caption = fig_caption,
+    highlight = highlight,
+    keep_tex = keep_tex,
     includes = rmarkdown::includes(
       in_header = doc_prefix,
-      before_body = doc_prebody,
       after_body = doc_afterbody
-    )
+    ),
+    pandoc_args = pandoc_args
   )
 }
 
